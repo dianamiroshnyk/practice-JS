be74182ec4621333c9231f653d9db86be6d625d2
dianamiroshnyk <89104528+dianamiroshnyk@users.noreply.github.com>
2023-06-28 02:44:13 +0300

    Add files via upload

---
+++ objectCar/script.js
@@ -0,0 +1,14 @@
+const Car = {
+    brand: 'Mitsubishi',
+    model: 'Outlander Sport',
+    color: 'Red',
+    year: '2023',
+    fuelType: 'Petrol',
+
+  };
+  
+console.log(Car.brand); 
+console.log(Car.model);  
+console.log(Car.color);
+console.log(Car.year);
+console.log(Car.fuelType);
