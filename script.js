0d57dbb17ae70bff4a2bb1fdaa51c68404be6509
dianamiroshnyk <89104528+dianamiroshnyk@users.noreply.github.com>
2023-06-28 02:33:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function getMul (){
+    let a = document.getElementById("a").value;
+    let b = document.getElementById("b").value;
+    let mul = (num1, num2) => num1 * num2;
+    alert("Результат множення: " + mul(a, b));
+}
+
